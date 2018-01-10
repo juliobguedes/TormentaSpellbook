5b82b95f079a176040d7279e46ffeb85bf3a313b
juliobguedes <julio.costa@ccc.ufcg.edu.br>
2018-01-10 08:50:16 -0300

    fully functional, icon added

--- src/controllers/oneSpellCtrl.js
+++ src/controllers/oneSpellCtrl.js
@@ -2,6 +2,7 @@ app.controller("oneSpellCtrl", function($scope, url, $showdown, $http) {
     let newUrl = "https://raw.githubusercontent.com/juliobguedes/spells.md/master/" + url;
     $http.get(newUrl)
         .then(res => {
+            console.log(newUrl);
             $scope.spell = $showdown.makeHtml(res.data);
         }).catch(error => {
             console.log("error");
